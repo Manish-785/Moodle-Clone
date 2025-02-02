905933da4c4d1ab728db7d3a80772a5e70678292
Darth-Darsh <odiosisdarsh@gmail.com>
2025-02-02 17:25:23 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,23 @@ import NavBar from './NavBar/NavBar.jsx'
 import Header from './header/header.jsx'
 import RotatingCards from './rotatingcards/rotatingcards.jsx'
 import Footer from './footer/footer.jsx'
-// import Signup from './signup/signup.jsx'
-// import Landing from './landing/landing.jsx'
-// import Profile from './profile/profile.jsx'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min.js'
 
 function App() {
   return(
-    <div className='app'>
-      <NavBar></NavBar>
-      <Header course="CS666"></Header>
-     <RotatingCards></RotatingCards>
-     <Footer></Footer>
-     {/* <Landing></Landing> 
-     <Signup></Signup> 
-     <Profile></Profile> */}
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/course">
+          <div className='app'>
+          <NavBar></NavBar>
+          <Header course="CS666"></Header>
+          <RotatingCards></RotatingCards>
+          <Footer></Footer>
+          </div>
+        </Route>
+     </Switch>
+    </Router>
   )
   
 }
