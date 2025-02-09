0f15be2c750a069c1ebb2514baafaaba03d0a0a7
Darth-Darsh <odiosisdarsh@gmail.com>
2025-02-09 20:23:39 +0530

    uygy

--- src/rotatingcards/rotatingcards.jsx
+++ src/rotatingcards/rotatingcards.jsx
@@ -1,12 +1,8 @@
 import './rotatingcards.css'
 import React, { useState } from "react";
-<<<<<<< Updated upstream
-function RotatingCards(){
-=======
 
 function RotatingCards(props){
->>>>>>> Stashed changes
-    const cards = ["Card 1", "Card 2", "Card 3", "Card 4", "Card 5"];
+  const cards = ["Card 1", "Card 2", "Card 3", "Card 4", "Card 5"];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNext = () => {
@@ -14,16 +10,12 @@ function RotatingCards(props){
   };
 
   return (
-<<<<<<< Updated upstream
-    <div className="carousel-container">
-=======
     <div className="carousel-container" style={{
       marginLeft: props.isOpen ? '250px' : '0',
       transition: 'margin-left 0.3s ease-in-out',
       padding: '20px',
     }}>
       <h3 id="tag">{props.heading}</h3>
->>>>>>> Stashed changes
       <div className="card-stack">
         {cards.map((card, index) => {
           const position = (index - currentIndex + cards.length) % cards.length;
